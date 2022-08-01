23b12547efc7e8143ed5ba9d1d9f3e151b5c792c
icecliffs <security@icecliffs.cn>
2022-08-01 22:43:55 +0800

    Basic Framework

---
+++ assert/easyjs.js
@@ -0,0 +1,11 @@
+// flag{QQ:378768535}
+$('li a').click(function () {
+     target = $(this).attr('goto');
+     switchTo(target);
+});
+function switchTo(target) {
+     $('.row section').each(function () {
+         $(this).removeClass('active');
+     });
+     $(target).addClass('active');
+}
