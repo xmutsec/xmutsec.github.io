ff3ae61e329ba8d31b69b42dab291d0e0f4d0010
icecliffs <security@icecliffs.cn>
2022-08-02 00:44:31 +0800

    Add Vue func

--- assert/easyjs.js
+++ assert/easyjs.js
@@ -9,3 +9,74 @@ function switchTo(target) {
      });
      $(target).addClass('active');
 }
+
+var team = new Vue({
+     el: '#members',
+     data: {
+          object:[
+               {
+                    date: "2022",
+                    members: ['Test1', 'Test2', 'Test3']
+               },
+               {
+                    date: "2021",
+                    members: ['Test1', 'Test2', 'Test3', 'Test4']
+               },
+               {
+                    date: "2019",
+                    members: ['Test1', 'Test2', 'Test3', 'Test4', 'Test5']
+               },
+               {
+                    date: "2019",
+                    members: ['Test1', 'Test2', 'Test3', 'Test4', 'Test5']
+               },
+               {
+                    date: "2019",
+                    members: ['Test1', 'Test2', 'Test3', 'Test4', 'Test5']
+               },
+               {
+                    date: "2019",
+                    members: ['Test1', 'Test2', 'Test3', 'Test4', 'Test5']
+               },
+               {
+                    date: "2019",
+                    members: ['Test1', 'Test2', 'Test3', 'Test4', 'Test5']
+               },
+               {
+                    date: "2019",
+                    members: ['Test1', 'Test2', 'Test3', 'Test4', 'Test5']
+               }
+          ]
+     },
+     // cnm报你妈错呢傻逼
+     methods: {
+          even: function (numbers) {
+               return numbers.filter(function (number) {
+                    return number
+               })
+          }
+     }
+})
+
+var award = new Vue({
+     el: '#award',
+     data: {
+          object:[
+               {
+                    date: "2022",
+                    members: [
+                         '2022年最佳吃饭大赛总决赛总冠军', 
+                         '中国互联网应急响应中心最佳封禁IP单位'
+                    ]
+               },
+          ]
+     },
+     // cnm报你妈错呢傻逼
+     methods: {
+          even: function (numbers) {
+               return numbers.filter(function (number) {
+                    return number
+               })
+          }
+     }
+})
